dac24c09be6707dbfd5014460c7a1bcac48b4071
Jae-Nadine <jae.nadine@icloud.com>
2024-08-04 00:50:00 -0700

    Initial commit

---
+++ src/com/assignment4/CourseManager.java
@@ -0,0 +1,10 @@
+package com.assignment4;
+
+public class CourseManager {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/assignment4/Student.java
@@ -0,0 +1,9 @@
+package com.assignment4;
+
+public class Student {
+	private int studentID;
+	private String studentName;
+	private String course;
+	private int grade;
+
+}
